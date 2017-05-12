c6509d7e36f8c0b5f8cca2646e1a659547eddca6
Saran Tanpituckpong <sarunta@gmail.com>
2017-05-12 23:22:15 +0700

    :arrow_up: Upgrade dependencies

--- dist/font-awesome-openui5.js
+++ dist/font-awesome-openui5.js
@@ -117,8 +117,8 @@ module.exports = isNil;
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/// <reference types="font-awesome-icon-chars" />
 
+/// <reference types="font-awesome-icon-chars" />
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
