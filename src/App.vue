7d177e8a04dcfb0c7efa5071c9e6ccb3e8537de1
Saran Tanpituckpong <sarunta@gmail.com>
2018-11-11 17:08:11 +0700

    :bug: Fix menu active navigation bug

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,23 @@
 	Menu(
 		mode='horizontal'
 		:active-name='currentMenuName'
-		@on-select='onMenuSelect'
 	)
-		MenuItem(name='home')
+		MenuItem(name='home' to='/')
 			Icon(type='md-home' size='20')
 			| Home
-		MenuItem(name='start')
+		MenuItem(name='start' to='/start')
 			Icon(type='md-play' size='20')
 			| Get started
-		MenuItem(name='api')
+		MenuItem(name='api' to='/api')
 			Icon(type='md-document' size='20')
 			| API
 		Submenu(name='demo')
 			template(slot='title')
 				Icon(type='md-desktop' size='20')
 				| Demo
-			MenuItem(name='demo-solid') Solid Icons
-			MenuItem(name='demo-regular') Regular Icons
-			MenuItem(name='demo-brands') Brands Icons
+			MenuItem(name='demo-solid' to='/demo/solid') Solid Icons
+			MenuItem(name='demo-regular' to='/demo/regular') Regular Icons
+			MenuItem(name='demo-brands' to='/demo/brands') Brands Icons
 	router-view
 	footer
 		p Made with #[Icon(type='md-heart' size='30' color='red')] by #[BlankLink(url='https://github.com/gluons') Saran Tanpituckpong]
@@ -28,33 +27,16 @@
 
 
 <script>
-const demoNameRegex = /^(demo)-([a-z]+)/;
 const demoPathRegex = /^\/(demo)\/([a-z]+)/;
 
 function pathToMenuName(path) {
 	if (demoPathRegex.test(path)) {
-		let matches = path.match(demoPathRegex);
-		let demo = matches[1];
-		let style = matches[2];
+		const [, demo, style] = path.match(demoPathRegex);
+
 		return `${demo}-${style}`;
 	} else {
 		return path !== '/' ? path.substr(1) : 'home';
 	}
-}
-
-function menuNameToPath(menuName) {
-	let path;
-	if (menuName === 'home') {
-		path = '/';
-	} else if (demoNameRegex.test(menuName)) {
-		let matches = menuName.match(demoNameRegex);
-		let demo = matches[1];
-		let style = matches[2];
-		path = `/${demo}/${style}`;
-	} else {
-		path = `/${menuName}`;
-	}
-	return path;
 }
 
 export default {
@@ -65,19 +47,13 @@ export default {
 		};
 	},
 	created() {
-		this.$router.beforeEach((to, from, next) => {
-			let path = to.path;
+		this.currentMenuName = pathToMenuName(this.$route.path);
+		this.$router.afterEach(({ path }) => {
 			this.currentMenuName = pathToMenuName(path);
-
-			next();
 		});
-		this.currentMenuName = pathToMenuName(this.$router.currentRoute.path);
 	},
-	methods: {
-		onMenuSelect(menuName) {
-			let path = menuNameToPath(menuName);
-			this.$router.push(path);
-		}
+	updated() {
+		this.currentMenuName = pathToMenuName(this.$route.path);
 	}
 };
 </script>
